f4162b51db9af50a9716864f0c7a5cb721cc9974
kimlulz <kimlulz@naver.com>
2021-03-24 17:43:03 +0900

    Changed wine install method

--- install PsCC-Linux.sh
+++ install PsCC-Linux.sh
@@ -8,12 +8,13 @@ echo "S C R I P T   B Y   K I M L U L Z"
 echo "github.com/kimlulz/lulzbuntu${normal}"
 echo "### Install Wine"
 sudo apt update
-sudo apt install -y wine64
+sudo dpkg --add-architecture i386 
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+sudo apt install --install-recommends winehq-stable
 echo "### Install Additional extras of Wine"
-sudo apt install -y  winetricks mono-complete
-wget http://dl.winehq.org/wine/wine-gecko/2.47.2/wine-gecko-2.47.2-x86_64.tar.xz
-tar -xvf wine-gecko-2.47.2-x86_64.tar.xz 
-sudo mv wine-gecko-2.47.2-x86_64  /usr/share/wine/gecko
+sudo apt install -y  winetricks
 echo "### Download PsCC-Linux from Github"
 git clone https://github.com/Gictorbit/photoshopCClinux.git
 cd photoshopCClinux
