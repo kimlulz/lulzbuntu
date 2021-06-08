9a14eec126a45b6653e103660491be0cd4abcaf3
kimlulz <kimlulz@naver.com>
2021-06-08 16:57:12 +0900

    Updated ZSH

--- zsh_install.sh
+++ zsh_install.sh
@@ -5,7 +5,7 @@ echo ""
 sudo apt install -y zsh
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh install.sh --unattended
-sudo apt install -y fonts-powerline
 #!/bin/zsh
+sudo apt install -y fonts-powerline
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
 #chsh -s /usr/bin/zsh
