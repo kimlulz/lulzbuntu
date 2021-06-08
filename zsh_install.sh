3dee2d35900309df9bcc7bfa459517da26ed52b2
kimlulz <kimlulz@naver.com>
2021-06-08 16:25:44 +0900

    update

--- zsh_install.sh
+++ zsh_install.sh
@@ -3,8 +3,8 @@ echo "WIP !DO NOT USE!"
 echo ""
 echo ""
 sudo apt install -y zsh
-git clone --depth=1 git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp -f .zshrc ~/
-sudo apt install fonts-powerline
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+sudo apt install -y fonts-powerline
 zsh
 #chsh -s /usr/bin/zsh
