f2ed3f17c6dc9364d5c82dd282415d944d4f7981
kimlulz <42508318+kimlulz@users.noreply.github.com>
2021-11-09 17:09:15 +0900

    Fixed Script

--- zsh_install.sh
+++ zsh_install.sh
@@ -12,7 +12,7 @@ mv MesloLGS*.ttf ~/.local/share/fonts/
 fc-cache -f -v
 echo "## Install Plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zdharma/fast-syntax-highlighting.git \
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 sed -i 's/plugins=(git)/plugins=(git fast-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
